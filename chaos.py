cc947c4b23c9af2068e16bf74cc0fb6d177d8133
EvanDavis <evan.davis1914@gmail.com>
2016-10-05 20:56:36 -0500

    range mod

--- chaos.py
+++ chaos.py
@@ -4,7 +4,7 @@
 def main():
     print('This program illustrates a chaotic function.')
     x = eval(input('Enter a number between 0 and 1'))
-    for i in range (10):
+    for i in range (20):
         x = 2.0 * x * (1 - x)
         print(x)
 
